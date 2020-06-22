12e9761e32f794d33527d1e65b857f0a31f64c9a
random-person01 <66160850+random-person01@users.noreply.github.com>
2020-06-22 20:25:28 +1200

    Update financial.py

--- system/components/financial.py
+++ system/components/financial.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from matplotlib import pyplot as plt
 
-filename = 'sitka_weather_2018_simple.csv'
+filename = 'income.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
